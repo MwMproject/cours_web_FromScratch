f26072024819f26baccc8c5eeb8b505668cf596a
Manu <mweiss1377@gmail.com>
2025-10-16 20:27:40 +0200

    add counterDisplay function to index.js and h3 tag to index.html / TP finished

--- JavaScript/TP_bubble_generator/assets/index.js
+++ JavaScript/TP_bubble_generator/assets/index.js
@@ -1,3 +1,6 @@
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
+
 const bubbleMaker = () => {
   const bubble = document.createElement("span"); // Création d'un élément span
   bubble.classList.add("bubble"); // Ajout de la classe "bubble"
@@ -14,9 +17,15 @@ const bubbleMaker = () => {
   const plusMinus = Math.random() < 0.5 ? -1 : 1; // Choix aléatoire entre -1 et 1 pour la direction de l'animation
   bubble.style.setProperty("--left", Math.random() * 100 + plusMinus + "%"); // Définition de la variable CSS --left pour les animations
 
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter; // Mise à jour du compteur affiché
+    bubble.remove(); // Suppression de la bulle au clic
+  });
+
   setTimeout(() => {
     bubble.remove(); // Suppression de l'élément après l'animation (gérée en CSS)
   }, 8000); // Durée de vie de la bulle avant suppression
 };
 
-setInterval(bubbleMaker, 3000); // Appel de la fonction bubbleMaker toutes les 300 millisecondes
+setInterval(bubbleMaker, 1000); // Appel de la fonction bubbleMaker toutes les 300 millisecondes
