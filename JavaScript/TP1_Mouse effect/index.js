a573a006b579a0cacbf0d69234adcd0d550292ba
Manu <mweiss1377@gmail.com>
2025-10-18 20:11:08 +0200

    add optimized mouse effect js code

--- JavaScript/TP1_Mouse effect/index.js
+++ JavaScript/TP1_Mouse effect/index.js
@@ -1,8 +1,19 @@
+//vésrion optimisée en utilisant forEach et querySelectorAll
+const mouses = document.querySelectorAll(".mouse");
+
 window.addEventListener("mousemove", (e) => {
-  cursor.style.top = e.pageY + "px";
-  cursor.style.left = e.pageX + "px";
-  mouse1.style.top = e.pageY + "px";
-  mouse1.style.left = e.pageX + "px";
-  mouse2.style.top = e.pageY + "px";
-  mouse2.style.left = e.pageX + "px";
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px";
+  });
 });
+
+//Vérsion simple
+//window.addEventListener("mousemove", (e) => {
+//cursor.style.top = e.pageY + "px";
+//cursor.style.left = e.pageX + "px";
+//mouse1.style.top = e.pageY + "px";
+//mouse1.style.left = e.pageX + "px";
+//mouse2.style.top = e.pageY + "px";
+//mouse2.style.left = e.pageX + "px";
+//});
