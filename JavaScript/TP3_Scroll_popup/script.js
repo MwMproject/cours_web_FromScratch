5fd69ab1575700fcd1f39668b8ce5e6e655f629c
Manu <mweiss1377@gmail.com>
2025-10-20 19:07:36 +0200

    add scroll event listener + img effect

--- JavaScript/TP3_Scroll_popup/script.js
+++ JavaScript/TP3_Scroll_popup/script.js
@@ -1,6 +1,23 @@
+window.addEventListener("scroll", function () {
+  // navbar effect
+  if (window.scrollY > 50) {
+    navbar.style.height = "45px";
+  } else {
+    navbar.style.height = "80px";
+  }
+
+  // image improvise effect
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / this.document.body.offsetHeight;
+  if (scrollValue > 0.6) {
+    imgImprovise.style.opacity = "1";
+    imgImprovise.style.transform = "none"; //  ou  translateY(0)
+  }
+});
+
 // Créer un événement au scroll
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+// Réduire la navbar quand on descend vers le bas, la remettre à sa taille initiale si on remonte tout en haut
 
 // Faire apparaitre l'image de la partie improvise
 
