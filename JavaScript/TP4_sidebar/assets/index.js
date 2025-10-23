1a9b914495536a9f24c16bc317d0f9b16f49ab0f
Manu <mweiss1377@gmail.com>
2025-10-23 13:44:48 +0200

    add transformation hamburger to cross menu

--- JavaScript/TP4_sidebar/assets/index.js
+++ JavaScript/TP4_sidebar/assets/index.js
@@ -1,14 +1,12 @@
 const sidebar = document.getElementById("side-bar");
-
+const content = document.querySelector(".content");
+// Ouvrir et fermer la sidebar au click sur le bouton
 btn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
 });
-// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+// fermer la sidebar si on click sur le contenu principal
+content.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
 
-// Ranger la sidebar si on click sur le contenu principal
-
-// BONUS
-// Créer en css l'hamburger menu qui se transforme en croix
-
-// Aide
-// Priorités en CSS : id > classe > baliseHtml
+// Priorités en CSS : !important > id > classe > baliseHtml
