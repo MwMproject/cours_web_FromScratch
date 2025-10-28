0c516c9adac64c51ee66aa751de6a9bf5c041b0c
Manu <mweiss1377@gmail.com>
2025-10-28 18:49:54 +0100

    add color generator script for rbg colors text boxes

--- JavaScript/TP5_Color_generator/assets/script.js
+++ JavaScript/TP5_Color_generator/assets/script.js
@@ -1,9 +1,26 @@
 function rgbGenerator() {
-  const r = Math.floor(Math.random() * 256);
+  const r = Math.floor(Math.random() * 256); // 0-255
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
   let rgb = `rgb(${r}, ${g}, ${b})`;
   document.body.style.backgroundColor = rgb;
+
+  const rgbText = document.createElement("div");
+  rgbText.style.position = "absolute";
+  rgbText.style.top = "50%";
+  rgbText.style.left = "50%";
+  rgbText.style.transform = "translate(-50%, -50%)";
+  rgbText.style.fontSize = "2em";
+  rgbText.style.fontFamily = "Arial, sans-serif";
+  rgbText.style.color = "#ffffff";
+  rgbText.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  rgbText.style.borderRadius = "10px";
+  rgbText.style.padding = "20px 20px";
+  rgbText.innerText = rgb;
+
+  document.body.innerHTML = "";
+  document.body.appendChild(rgbText);
 }
 
 rgbGenerator();
+setInterval(rgbGenerator, 3000);
