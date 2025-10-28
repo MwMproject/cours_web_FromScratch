ac62d0e26f55d8c7683da39504e0592b7ea9acd6
Manu <mweiss1377@gmail.com>
2025-10-28 19:04:18 +0100

    add from scratch's version

--- JavaScript/TP5_Color_generator/assets/script.js
+++ JavaScript/TP5_Color_generator/assets/script.js
@@ -1,3 +1,4 @@
+// Fonction pour générer une couleur RGB aléatoire et mettre à jour le fond de la page
 function rgbGenerator() {
   const r = Math.floor(Math.random() * 256); // 0-255
   const g = Math.floor(Math.random() * 256);
@@ -5,6 +6,7 @@ function rgbGenerator() {
   let rgb = `rgb(${r}, ${g}, ${b})`;
   document.body.style.backgroundColor = rgb;
 
+  // Afficher le code RGB au centre de la page + ajout de style css pour le texte
   const rgbText = document.createElement("div");
   rgbText.style.position = "absolute";
   rgbText.style.top = "50%";
@@ -18,9 +20,20 @@ function rgbGenerator() {
   rgbText.style.padding = "20px 20px";
   rgbText.innerText = rgb;
 
-  document.body.innerHTML = "";
-  document.body.appendChild(rgbText);
+  document.body.innerHTML = ""; // nettoyer le contenu précédent
+  document.body.appendChild(rgbText); // ajouter le nouveau texte
 }
+rgbGenerator(); // Appel initial pour afficher une couleur dès le chargement de la page
+setInterval(rgbGenerator, 3000); // Mettre à jour la couleur toutes les 3 secondes
 
-rgbGenerator();
-setInterval(rgbGenerator, 3000);
+// Vérsion From Scratch :
+// setInterval(() => {
+//const r = Math.floor(Math.random() * 255); // 0-255
+// const g = Math.floor(Math.random() * 255);
+// const b = Math.floor(Math.random() * 255);
+
+// const color = `rgb(${r}, ${g}, ${b})`;
+
+// document.body.style.backgroundColor = color;
+// document.body.innerText = `<h2>${color}</h2>`;
+// }, 3000);
