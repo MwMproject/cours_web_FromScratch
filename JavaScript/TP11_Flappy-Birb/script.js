2fab1c3a620fa2c36d976e7cf2c73be46c66a610
Manu <mweiss1377@gmail.com>
2026-03-26 18:30:12 +0100

    add "Flappy Birb" game script general and pipe settings

--- JavaScript/TP11_Flappy-Birb/script.js
+++ JavaScript/TP11_Flappy-Birb/script.js
@@ -1,1 +1,39 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const img = new Image();
+img.src = "./media/flappy-bird-set.png";
 
+// general settings
+let gamePlaying = false;
+const gravity = 0.5;
+const speed = 6.2;
+const size = [51, 36];
+const jump = -11.5;
+const cTenth = canvas.width / 10;
+
+let index = 0,
+  bestScore = 0,
+  flight,
+  flyHeight,
+  currentScore,
+  pipe;
+
+// pipe settings
+const pipeWidth = 78;
+const pipeGap = 270;
+const pipeLoc = () =>
+  Math.random() * (canvas.height - (pipeGap + pipeWidth) - pipeWidth) +
+  pipeWidth;
+
+const setup = () => {
+  currentScore = 0;
+  flight = jump;
+
+  // set initial flyHeight (middle of screen - size of the bird)
+  flyHeight = canvas.height / 2 - size[1] / 2;
+
+  // setup first 3 pipes
+  pipes = Array(3)
+    .fill()
+    .map((a, i) => [canvas.width + i * (pipeGap + pipeWidth), pipeLoc()]);
+};
